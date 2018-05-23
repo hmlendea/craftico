018bf44e426a6376a0525e42f304d9f309042c53
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-05-23 18:20:04 +0300

    Added drawing of mob equipment

--- Craftico.Gui/GuiElements/GuiMob.cs
+++ Craftico.Gui/GuiElements/GuiMob.cs
@@ -12,8 +12,15 @@ namespace Craftico.Gui.GuiElements
 {
     public class GuiMob : GuiElement
     {
-        Sprite mob;
-        HumanSpriteSheetEffect humanEffect;
+        Sprite bodyAppearance;
+        Sprite headEquipment;
+        Sprite handEquipment;
+        Sprite torsoEquipment;
+
+        HumanSpriteSheetEffect bodyAppearanceEffect;
+        HumanSpriteSheetEffect headEquipmentEffect;
+        HumanSpriteSheetEffect handEquipmentEffect;
+        HumanSpriteSheetEffect torsoEquipmentEffect;
 
         public MobAction Action { get; set; }
 
@@ -21,61 +28,110 @@ namespace Craftico.Gui.GuiElements
 
         public override void LoadContent()
         {
-            humanEffect = new HumanSpriteSheetEffect();
-            mob = new Sprite
+            bodyAppearanceEffect = new HumanSpriteSheetEffect();
+            headEquipmentEffect = new HumanSpriteSheetEffect();
+            handEquipmentEffect = new HumanSpriteSheetEffect();
+            torsoEquipmentEffect = new HumanSpriteSheetEffect();
+
+            bodyAppearance = new Sprite
             {
                 ContentFile = "SpriteSheets/Mobs/human_male_white",
-                SpriteSheetEffect = humanEffect,
+                SpriteSheetEffect = bodyAppearanceEffect,
+                Active = true
+            };
+            headEquipment = new Sprite
+            {
+                ContentFile = "SpriteSheets/Equipment/leather_cap_male",
+                SpriteSheetEffect = headEquipmentEffect,
+                Active = true
+            };
+            handEquipment = new Sprite
+            {
+                ContentFile = "SpriteSheets/Equipment/metal_gloves_male",
+                SpriteSheetEffect = handEquipmentEffect,
+                Active = true
+            };
+            torsoEquipment = new Sprite
+            {
+                ContentFile = "SpriteSheets/Equipment/leather_chest_male",
+                SpriteSheetEffect = torsoEquipmentEffect,
                 Active = true
             };
 
-            mob.SpriteSheetEffect.AssociateSprite(mob);
-            mob.LoadContent();
+            bodyAppearance.SpriteSheetEffect.AssociateSprite(bodyAppearance);
+            headEquipment.SpriteSheetEffect.AssociateSprite(headEquipment);
+            handEquipment.SpriteSheetEffect.AssociateSprite(handEquipment);
+            torsoEquipment.SpriteSheetEffect.AssociateSprite(torsoEquipment);
+
+            bodyAppearance.LoadContent();
+            headEquipment.LoadContent();
+            handEquipment.LoadContent();
+            torsoEquipment.LoadContent();
 
             base.LoadContent();
 
-            mob.SpriteSheetEffect.Activate();
+            bodyAppearance.SpriteSheetEffect.Activate();
+            headEquipment.SpriteSheetEffect.Activate();
+            handEquipment.SpriteSheetEffect.Activate();
+            torsoEquipment.SpriteSheetEffect.Activate();
         }
 
         public override void UnloadContent()
         {
             base.UnloadContent();
 
-            mob.UnloadContent();
+            bodyAppearance.UnloadContent();
+            headEquipment.UnloadContent();
+            handEquipment.UnloadContent();
+            torsoEquipment.UnloadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
 
-            mob.Update(gameTime);
+            bodyAppearance.Update(gameTime);
+            headEquipment.Update(gameTime);
+            handEquipment.Update(gameTime);
+            torsoEquipment.Update(gameTime);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
 
-            mob.Draw(spriteBatch);
+            bodyAppearance.Draw(spriteBatch);
+            headEquipment.Draw(spriteBatch);
+            handEquipment.Draw(spriteBatch);
+            torsoEquipment.Draw(spriteBatch);
         }
 
         protected override void SetChildrenProperties()
         {
             base.SetChildrenProperties();
 
-            humanEffect.Action = Action;
-            humanEffect.Direction = Direction;
+            bodyAppearanceEffect.Action = Action;
+            headEquipmentEffect.Action = Action;
+            handEquipmentEffect.Action = Action;
+            torsoEquipmentEffect.Action = Action;
 
-            if (mob.SourceRectangle.Width == GameDefines.MAP_TILE_SIZE &&
-                mob.SourceRectangle.Height == GameDefines.MAP_TILE_SIZE)
-            {
-                mob.Location = Location;
-            }
-            else
-            {
-                mob.Location = new Point2D(
-                    Location.X + (GameDefines.MAP_TILE_SIZE - mob.SourceRectangle.Width) / 2,
-                    Location.Y + GameDefines.MAP_TILE_SIZE - mob.SourceRectangle.Height);
-            }
+            bodyAppearanceEffect.Direction = Direction;
+            headEquipmentEffect.Direction = Direction;
+            handEquipmentEffect.Direction = Direction;
+            torsoEquipmentEffect.Direction = Direction;
+
+            bodyAppearance.Location = new Point2D(
+                Location.X + (GameDefines.MAP_TILE_SIZE - bodyAppearance.SourceRectangle.Width) / 2,
+                Location.Y + GameDefines.MAP_TILE_SIZE - bodyAppearance.SourceRectangle.Height);
+            headEquipment.Location = new Point2D(
+                Location.X + (GameDefines.MAP_TILE_SIZE - headEquipment.SourceRectangle.Width) / 2,
+                Location.Y + GameDefines.MAP_TILE_SIZE - headEquipment.SourceRectangle.Height);
+            handEquipment.Location = new Point2D(
+                Location.X + (GameDefines.MAP_TILE_SIZE - handEquipment.SourceRectangle.Width) / 2,
+                Location.Y + GameDefines.MAP_TILE_SIZE - handEquipment.SourceRectangle.Height);
+            torsoEquipment.Location = new Point2D(
+                Location.X + (GameDefines.MAP_TILE_SIZE - torsoEquipment.SourceRectangle.Width) / 2,
+                Location.Y + GameDefines.MAP_TILE_SIZE - torsoEquipment.SourceRectangle.Height);
         }
     }
 }
